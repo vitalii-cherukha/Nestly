a312f9dbb7c0b0a1a8ab4c289fdf26c382a2451b
Oleg Shapoval <olegkazhan@gmail.com>
2025-09-12 14:57:28 +0300

    finish template

---
+++ components/Container/Container.tsx
@@ -0,0 +1,4 @@
+import css from "./Container.module.css";
+export default function Container() {
+  return <div className={css.container}></div>;
+}
---
+++ components/Section/Section.tsx
@@ -0,0 +1,5 @@
+import css from "./Section.module.css";
+
+export default function Section() {
+  return <div className={css.section}></div>;
+}
