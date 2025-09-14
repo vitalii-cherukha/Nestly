4c571c2f068cc3c97e0087245535c682b151a039
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-09-14 14:49:14 +0300

    init-profile

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,16 +1,18 @@
 import Container from "@/components/Container/Container";
-//import ProfileAvatar from "@/components/ProfileAvatar/ProfileAvatar";
-//import ProfileEditForm from "@/components/ProfileEditForm/ProfileEditForm";
-import Section from "@/components/Section/Section";
+import ProfileAvatar from "@/components/ProfileAvatar/ProfileAvatar";
 
-const ProfilePage = () => {
+import ProfileEditForm from "@/components/ProfileEditForm/ProfileEditForm";
+import Section from "@/components/Section/Section";
+import { getServerMe } from "@/lib/api/serverApi";
+
+const ProfilePage = async () => {
+  const user = await getServerMe();
   return (
     <main>
       <Section>
         <Container>
-          Hello
-          {/* <ProfileAvatar /> */}
-          {/* <ProfileEditForm /> */}
+          <ProfileAvatar user={user} />
+          <ProfileEditForm user={user} />
         </Container>
       </Section>
     </main>
