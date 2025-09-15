e053b586a518a3f82a018d6ffcaf9c1bfc14ad2b
Djakyz <djakyz@gmail.com>
2025-09-15 12:34:27 +0300

    add meta

--- app/(private routes)/diary/[id]/page.tsx
+++ app/(private routes)/diary/[id]/page.tsx
@@ -5,10 +5,47 @@ import {
 } from "@tanstack/react-query";
 import { getDiaryListServer } from "@/lib/api/serverApi";
 import DiaryEntryDetails from "@/components/DiaryEntryDetails/DiaryEntryDetails";
+import { Metadata } from "next";
+
+// MetaData
 
 interface DiaryIdPageProps {
   params: Promise<{ id: string }>;
 }
+
+export const generateMetadata = async ({
+  params,
+}: DiaryIdPageProps): Promise<Metadata> => {
+  const { id } = await params;
+
+  const { diaryNotes } = await getDiaryListServer();
+  const note = diaryNotes.find((n) => n._id === id);
+
+  if (!note) {
+    return {
+      title: "Запис не знайдено",
+      description: "Цей запис не існує",
+    };
+  }
+
+  return {
+    title: note.title,
+    description: note.description.slice(0, 15),
+    openGraph: {
+      title: note.title,
+      description: note.description.slice(0, 15),
+      url: `https://nestly-alpha.vercel.app/diary/${id}`,
+      images: [
+        {
+          url: "../../../public/logo.png",
+          width: 120,
+          height: 120,
+          alt: note.title,
+        },
+      ],
+    },
+  };
+};
 
 export default async function DiaryEntryPage({ params }: DiaryIdPageProps) {
   // Розпаковуємо params асинхронно для Некста
