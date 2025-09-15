3acfaea779cd7d77068b5a0ca18aa123398c1e11
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-09-15 15:10:59 +0300

    fix-profile

--- components/ProfileAvatar/ProfileAvatar.tsx
+++ components/ProfileAvatar/ProfileAvatar.tsx
@@ -1,60 +1,66 @@
 "use client";
 
-import { User } from "@/types/user";
 import css from "./ProfileAvatar.module.css";
 import Image from "next/image";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { ApiError } from "next/dist/server/api-utils";
+import { updateAvatar } from "@/lib/api/clientApi";
+import { useAuthStore } from "@/lib/store/authStore";
 
-interface ProfileAvatarProps {
-  user: User;
-}
+const ProfileAvatar = () => {
+  const user = useAuthStore((state) => state.user);
+  const setUser = useAuthStore((state) => state.setUser);
+  const [error, setError] = useState("");
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-const ProfileAvatar = ({ user }: ProfileAvatarProps) => {
-  //   const fileInputRef = useRef<HTMLInputElement | null>(null);
+  if (!user) {
+    return <p>Завантаження профілю...</p>;
+  }
 
-  //   const handleClick = () => {
-  //     fileInputRef.current?.click();
-  //   };
+  const handleClick = () => {
+    fileInputRef.current?.click();
+  };
 
-  //   const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-  //     const file = e.target.files?.[0];
-  //     if (!file) return;
-
-  //     console.log("Обране фото:", file);
-
-  //     const formData = new FormData();
-  //     formData.append("avatar", file);
-  //     await fetch("/api/upload-avatar", { method: "POST", body: formData });
-  //   };
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      try {
+        const updatedUser = await updateAvatar(file);
+        setUser({
+          ...user,
+          ...updatedUser,
+        });
+      } catch (error) {
+        setError((error as ApiError).message);
+      }
+    }
+  };
 
   return (
     <div className={css.container}>
       <Image
         src={user.avatarUrl}
         alt={user.name}
-        width={120}
-        height={120}
+        width={132}
+        height={132}
         className={css.avatarUser}
       />
       <div className={css.wrapper}>
         <h2 className={css.nameUser}>{user.name}</h2>
         <p className={css.emailUser}>{user.email}</p>
-        <button
-          //   onClick={handleClick}
-
-          type="button"
-          className={css.btn}
-        >
+        <button onClick={handleClick} type="button" className={css.btn}>
           Завантажити нове фото
         </button>
 
-        {/* <input
+        <input
           type="file"
           accept="image/*"
           ref={fileInputRef}
           style={{ display: "none" }}
           onChange={handleFileChange}
-        /> */}
+        />
+
+        {error && <p>{error}</p>}
       </div>
     </div>
   );
