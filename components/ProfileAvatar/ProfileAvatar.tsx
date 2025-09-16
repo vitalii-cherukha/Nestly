ef2f99e2c2a01caeb9800a283087d0d77b72498c
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-09-16 11:40:05 +0300

    style-profile-style

--- components/ProfileAvatar/ProfileAvatar.tsx
+++ components/ProfileAvatar/ProfileAvatar.tsx
@@ -11,6 +11,7 @@ const ProfileAvatar = () => {
   const user = useAuthStore((state) => state.user);
   const setUser = useAuthStore((state) => state.setUser);
   const [error, setError] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   if (!user) {
@@ -24,7 +25,25 @@ const ProfileAvatar = () => {
   const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
+      if (!file.type.startsWith("image/")) {
+        setError("Only Images");
+        return;
+      } else if (file.size > 1 * 1024 * 1024) {
+        setError("Max file size 1MB");
+        return;
+      }
       try {
+        setIsUploading(true);
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setUser({
+            ...user,
+            avatarUrl: reader.result as string,
+          });
+        };
+        reader.readAsDataURL(file);
+
         const updatedUser = await updateAvatar(file);
         setUser({
           ...user,
@@ -32,6 +51,8 @@ const ProfileAvatar = () => {
         });
       } catch (error) {
         setError((error as ApiError).message);
+      } finally {
+        setIsUploading(false);
       }
     }
   };
@@ -40,16 +61,17 @@ const ProfileAvatar = () => {
     <div className={css.container}>
       <Image
         src={user.avatarUrl}
-        alt={user.name}
+        alt={user.name || "User avatar"}
         width={132}
         height={132}
         className={css.avatarUser}
+        priority
       />
       <div className={css.wrapper}>
         <h2 className={css.nameUser}>{user.name}</h2>
         <p className={css.emailUser}>{user.email}</p>
         <button onClick={handleClick} type="button" className={css.btn}>
-          Завантажити нове фото
+          {isUploading ? "Завантажується..." : "Завантажити нове фото"}
         </button>
 
         <input
@@ -58,6 +80,7 @@ const ProfileAvatar = () => {
           ref={fileInputRef}
           style={{ display: "none" }}
           onChange={handleFileChange}
+          disabled={isUploading}
         />
 
         {error && <p>{error}</p>}
