22a83c42f76e81192eb42d791e636fac0a883cec
Olexander Motyl <JustSanya1@gmail.com>
2025-09-17 22:52:05 +0200

    fixed sidebar + layout styles, changed from grid to flex

--- app/(sidebar routes)/layout.tsx
+++ app/(sidebar routes)/layout.tsx
@@ -3,6 +3,7 @@
 import css from "./layout.module.css";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
+import Header from "@/components/Header/Header";
 
 const LayoutClient = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -11,6 +12,7 @@ const LayoutClient = ({ children }: { children: React.ReactNode }) => {
         <Sidebar />
       </div>
       <main className={css.main}>
+        <Header />
         <Breadcrumbs />
         {children}
       </main>
