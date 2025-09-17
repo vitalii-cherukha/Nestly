5f26f784ee0ea02545885b49c2cfb54288666063
vitalii_cherukha <vitalii.cherukha@gmail.com>
2025-09-17 15:25:48 +0300

    optimization

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -1,19 +1,22 @@
 import Container from "@/components/Container/Container";
 import ProfileAvatar from "@/components/ProfileAvatar/ProfileAvatar";
-
 import ProfileEditForm from "@/components/ProfileEditForm/ProfileEditForm";
 import Section from "@/components/Section/Section";
+import { getServerMe } from "@/lib/api/serverApi";
+import css from "./ProfilePage.module.css";
 
 const ProfilePage = async () => {
+  const user = await getServerMe();
+
   return (
-    <main>
+    <div className={css.wrapper}>
       <Section>
         <Container>
-          <ProfileAvatar />
-          <ProfileEditForm />
+          <ProfileAvatar userServer={user} />
+          <ProfileEditForm userServer={user} />
         </Container>
       </Section>
-    </main>
+    </div>
   );
 };
 
