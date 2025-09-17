9569e864ddb1168e826148dcb8c889f4f56aec51
Oleksandr Dmytriev <sanderdsukr@gmail.com>
2025-09-17 20:08:56 +0300

    mobile header fixed

--- app/(sidebar routes)/layout.tsx
+++ app/(sidebar routes)/layout.tsx
@@ -3,18 +3,22 @@
 import css from "./layout.module.css";
 import Sidebar from "@/components/Sidebar/Sidebar";
 import Breadcrumbs from "@/components/Breadcrumbs/Breadcrumbs";
+import Header from "@/components/Header/Header";
 
 const LayoutClient = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className={css.layout}>
-      <div className={css.sidebarWrap}>
-        <Sidebar />
+    <>
+      <Header />
+      <div className={css.layout}>
+        <div className={css.sidebarWrap}>
+          <Sidebar />
+        </div>
+        <main className={css.main}>
+          <Breadcrumbs />
+          {children}
+        </main>
       </div>
-      <main className={css.main}>
-        <Breadcrumbs />
-        {children}
-      </main>
-    </div>
+    </>
   );
 };
 
