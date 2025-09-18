c51a2e9f8bcce20629e8837855e702b3aeb954ff
Oleg Shapoval <olegkazhan@gmail.com>
2025-09-18 14:42:13 +0300

    fix

--- app/(auth routes)/auth/layout.tsx
+++ app/(auth routes)/auth/layout.tsx
@@ -1,9 +1,19 @@
+"use client";
 import css from "./authLayout.module.css";
-
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Loader from "@/components/Loader/Loader";
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  useEffect(() => {
+    router.refresh();
+    setLoading(false);
+  }, [router]);
+
   return (
     <div className={css.authLayout}>
-      <main>{children}</main>
+      <>{loading ? <Loader /> : <main>{children}</main>}</>;
     </div>
   );
 };
